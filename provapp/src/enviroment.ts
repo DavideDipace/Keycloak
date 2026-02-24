51bb89626264a6109d84e387db4e33e57ba76076
DavideDipace <113579525+DavideDipace@users.noreply.github.com>
2026-02-24 10:47:16 +0000

    keycloak

---
+++ provapp/src/enviroment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    keycloak: {
+        url: 'https://scaling-space-fishstick-6qxrgj76p5gh5xp-8080.app.github.dev',
+        realm: 'prova',
+        clientId: 'provapp',
+        redirectUri: window.location.origin,
+    }
+};
